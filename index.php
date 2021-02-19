b9822d895c744d6f6e13b2b73d1c190c37501189
AT071998 <67892006+AT071998@users.noreply.github.com>
2021-02-19 21:52:02 +0530

    add foreach loop

--- index.php
+++ index.php
@@ -9,6 +9,15 @@
 		$credit =1000;
 		echo "<h1>Welcome to ".$name."!</h1>";
 		echo "<h1>You have $".$credit."in your wallet!</h1>";
+		$products['computer'] = 750;
+		$products['car'] = 15000;
+		$products['iphone'] = 1000;
+		$products['Toaster'] = 75;
+		foreach($products as $key=>$value){
+			echo "<p>A ".$key." costs $".$value."</p>";
+		}
+		
+
 	?>
 
 </body>
